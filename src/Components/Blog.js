5e19c6216edbf67cc2353ee948e6571cd169d841
Jaymart Padilla <jaymartpadilla.dev@gmail.com>
2024-02-26 11:05:09 +0800

    refactored blog

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,23 +1,10 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import {
-  Button,
-  Card,
-  Col,
-  Container,
-  Form,
-  FormControl,
-  InputGroup,
-  Pagination,
-  Row,
-} from "react-bootstrap";
-import {
-  blogData,
-  blogCategories,
-  blogRecentPosts,
-  blogTags,
-} from "../config/dummy-data";
+import { Button, Card, Col, Container, Pagination, Row } from "react-bootstrap";
+import { blogData } from "../config/dummy-data";
 import { paths } from "../config/paths";
+import { formatDate } from "../utils/formateDate";
+import BlogSidebar from "./BlogSidebar";
 
 const styles = {
   blogCard: css({ marginBottom: "2.75rem" }),
@@ -78,60 +65,6 @@ const styles = {
       padding: "0.5rem 1rem",
       zIndex: 10,
     },
-  }),
-
-  blogSidebar: css({
-    display: "flex",
-    flexDirection: "column",
-    gap: "1.5rem",
-
-    fontSize: "0.9rem",
-  }),
-
-  blogSidebarSectionTitle: css({
-    fontSize: "1.125rem",
-    fontWeight: 500,
-    marginBottom: "0.5rem",
-  }),
-
-  blogSidebarSearchInput: css({
-    fontSize: "inherit",
-  }),
-
-  blogSidebarSearchButton: css({
-    color: "white",
-    border: "none",
-    borderRadius: "0 0.25rem 0.25rem 0",
-  }),
-
-  blogSidebarCategories: css({
-    display: "flex",
-    flexDirection: "column",
-    gap: "0.5rem",
-
-    color: "#555",
-    fontWeight: 500,
-  }),
-
-  blogSidebarRecentPostContainer: css({
-    display: "flex",
-    flexDirection: "column",
-    gap: "1rem",
-  }),
-
-  blogSidebarRecentPost: css({
-    display: "flex",
-    gap: "1rem",
-  }),
-
-  blogSidebarRecentPostTitle: css({
-    fontWeight: 500,
-    color: "inherit",
-  }),
-
-  blogSidebarTag: css({
-    fontSize: "small",
-    padding: "0.25rem 0.75rem",
   }),
 };
 
@@ -210,120 +143,4 @@ function BlogPagination() {
       </Pagination.Item>
     </Pagination>
   );
-}
-
-function BlogSidebar() {
-  return (
-    <section className="p-3" css={styles.blogSidebar}>
-      <SearchSection />
-      <CategoriesSection />
-      <RecentPostSection />
-      <TagSection />
-    </section>
-  );
-}
-
-function SearchSection() {
-  return (
-    <Form>
-      <div css={styles.blogSidebarSectionTitle}>Search</div>
-      <InputGroup>
-        <FormControl
-          className="border-right-0"
-          aria-label="Search"
-          aria-describedby="basic-addon2"
-          css={styles.blogSidebarSearchInput}
-        />
-        <Button
-          variant="success"
-          className="accent-button px-3"
-          css={styles.blogSidebarSearchButton}
-        >
-          <i className="fa-solid fa-magnifying-glass" />
-        </Button>
-      </InputGroup>
-    </Form>
-  );
-}
-
-function CategoriesSection() {
-  return (
-    <div>
-      <div css={styles.blogSidebarSectionTitle}>Categories</div>
-      <div css={styles.blogSidebarCategories}>
-        {blogCategories.map((blogCategory, index) => {
-          return (
-            <span key={index}>
-              {blogCategory.title}{" "}
-              <span className="text-secondary font-weight-normal">
-                <small>({blogCategory.count})</small>
-              </span>
-            </span>
-          );
-        })}
-      </div>
-    </div>
-  );
-}
-
-function RecentPostSection() {
-  return (
-    <div>
-      <div css={styles.blogSidebarSectionTitle}>Recent Posts</div>
-      <div css={styles.blogSidebarRecentPostContainer}>
-        {blogRecentPosts.map((blogRecentPost, index) => {
-          const formattedDate = formatDate(blogRecentPost.date);
-          return (
-            <div key={index} css={styles.blogSidebarRecentPost}>
-              <a href="#">
-                <img
-                  className="normalized-image"
-                  src={blogRecentPost.img}
-                  alt={blogRecentPost.title}
-                  width={75}
-                  height={50}
-                />
-              </a>
-              <div className="d-flex flex-column">
-                <a href="#" css={styles.blogSidebarRecentPostTitle}>
-                  {blogRecentPost.title}
-                </a>
-                <span className="text-secondary font-weight-normal">
-                  <small className="font-italic">{formattedDate}</small>
-                </span>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
-}
-
-function TagSection() {
-  return (
-    <div>
-      <div css={styles.blogSidebarSectionTitle}>Tags</div>
-      {blogTags.map((blogTag, index) => {
-        return (
-          <Button
-            variant="outline-secondary"
-            className="mb-2 mr-2 rounded-0"
-            size="sm"
-            key={index}
-            css={styles.blogSidebarTag}
-          >
-            {blogTag}
-          </Button>
-        );
-      })}
-    </div>
-  );
-}
-
-// utils
-function formatDate(dateString) {
-  const date = new Date(dateString);
-  const options = { month: "short", day: "numeric", year: "numeric" };
-  return date.toLocaleDateString("en-US", options);
 }
